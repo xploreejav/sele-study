85dd0f8b5a1bb570f78d3e097dbe22faee5c86f2
Alisha Jose <alishabasil777@gmail.com>
2024-12-13 23:25:22 -0500

    Added browser login code

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,8 +1,11 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.lang.String;
 
@@ -10,11 +13,20 @@ import java.lang.String;
 public class LoginTest {
 String baseUrl = "https://magento.softwaretestingboard.com/";//url
     @Test
-    public void launchApp(){
+    public void launchApp() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get(baseUrl);
         driver.manage().window().maximize();
-        driver.quit();
+        WebElement lnkSignIn = driver.findElement(By.linkText("Sign In"));
+        lnkSignIn.click();
+        driver.findElement(By.name("login[username]")).sendKeys("alishajose34@gmail.com");
+        driver.findElement(By.name("login[password]")).sendKeys("Burnside@1210");
+        driver.findElement(By.id("send2")).click();
+        Thread.sleep(3000);
+        String expWelMsg = "Welcome, Alisha Jose!";
+        String actWelMsg = driver.findElement(By.className("logged-in")).getText();
+        Assert.assertEquals(actWelMsg,expWelMsg,"login failed");
+    driver.quit();
     }
 }
