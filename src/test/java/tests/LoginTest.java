f567e4af1626c6f9ff311ce932268d85bdf95e0f
Alisha Jose <alishabasil777@gmail.com>
2024-12-13 22:45:57 -0500

    Added browser launching code

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import java.lang.String;
+
+
+public class LoginTest {
+String baseUrl = "https://magento.softwaretestingboard.com/";
+    @Test
+    public void launchApp(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.quit();
+    }
+}
